ab06481085755e42fc5f9c990d150707250ca6f4
opmapraise <opmapraise@gmail.com>
2022-09-08 19:35:16 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Rerurn: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programmingis like building a multilingual puzzle");
+	return (0);
+}
