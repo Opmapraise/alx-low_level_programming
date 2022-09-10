ce58f8c81ea6f837c8622ce955e8b90814c35c68
opmapraise <opmapraise@gmail.com>
2022-09-10 00:05:15 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	fwrite(2, "and that poece of arts is useful\" - Dora Korpar, 2015-10-19\n",
-	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	write(2, "and that poece of arts is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
